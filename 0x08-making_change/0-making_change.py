a898cb9150c6624e2b583be61277833749927b72
caleb kibe <kiplagatcaleb2@gmail.com>
2024-09-18 16:04:32 +0300

    determines the fewest number of coins needed to meet a given amount total

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
+"""Making Change"""
+
 
 def makeChange(coins, total):
-    """ Determine the fewest number of coins needed
-        to meet a given amount total
-    """
+    """Making Changes"""
     if total <= 0:
         return 0
-
-    # Create a dp array to store the minimum number of coins for each amount up to 'total'
-    dp = [float('inf')] * (total + 1)
-
-    # Base case: 0 coins are needed to make a total of 0
-    dp[0] = 0
-
-    # Fill the dp array by trying each coin denomination for each amount
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    # If dp[total] is still 'inf', it means it's not possible to make the total with the given coins
-    return dp[total] if dp[total] != float('inf') else -1
+    elif total > 0:
+        newList = sorted(coins[:])
+        newList = list(reversed(newList))
+        count = 0
+        value = total + 0
+        index = 0
+        while value >= 0 and (index < len(newList)):
+            if value >= newList[index]:
+                value = value - newList[index]
+                count += 1
+            elif value < newList[index]:
+                index += 1
+        if index == len(newList):
+            if value != 0:
+                return -1
+            elif value == 0:
+                return count
